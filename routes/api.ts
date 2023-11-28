ce120377c406f24a97f78562894920ac8c126ed0
Leon™ <leontm.dev@gmail.com>
2023-11-28 16:29:44 +0100

    First commit

---
+++ routes/api.ts
@@ -0,0 +1,13 @@
+// Imports
+
+import Express from "express";
+
+// Presets
+
+const apiRouter = Express.Router();
+
+// Code
+
+// Exports
+
+export default apiRouter;
---
+++ server.ts
@@ -0,0 +1,32 @@
+// Imports
+
+import { env } from "process";
+import Express from "express";
+import path from "path";
+import cors from "cors";
+
+// Presets
+
+const app = Express();
+
+// Configs
+
+app.use(Express.urlencoded({ extended: true }));
+app.use(Express.json());
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    origin: "https://*.find-a.app/*",
+  })
+);
+app.listen(2000, () => {
+  console.log("Server running on port 2000");
+});
+
+// API-Imports
+
+import apiRouter from "./routes/api";
+
+// Code
+
+app.use("/api", apiRouter);
