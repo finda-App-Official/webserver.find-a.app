ddfd1f2455ae239bfb735a5df9257e528097bb1d
LeonTM <LAnneken09@gmail.com>
2023-12-11 10:26:19 +0100

    Changed name of event interface from event to LogEvent

--- apiTypes.ts
+++ apiTypes.ts
@@ -16,7 +16,7 @@ export interface Job {
     email: string;
   };
 }
-export interface Event {
+export interface LogEvent {
   user: {
     name: "rieke" | "lasse" | "leon" | "ines" | "philipp";
   };
