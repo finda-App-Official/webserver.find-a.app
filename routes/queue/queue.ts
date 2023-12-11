4806006f6ba3463ebef5e6518cc81e2470a8aed4
LeonTM <LAnneken09@gmail.com>
2023-12-11 11:03:55 +0100

    Added clear

--- routes/queue/queue.ts
+++ routes/queue/queue.ts
@@ -9,10 +9,12 @@ const queueRouter = Express.Router();
 // API-Imports
 
 import checkRouter from "./check";
+import clearRouter from "./clear";
 
 // Code
 
 queueRouter.use("/check", checkRouter);
+queueRouter.use("/clear", clearRouter);
 
 // Exports
 
