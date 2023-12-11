ac6e3d2404c5587b68e8e70c45c283bcd0bf468c
LeonTM <LAnneken09@gmail.com>
2023-12-11 12:27:37 +0100

    Imported

--- routes/api.ts
+++ routes/api.ts
@@ -9,10 +9,12 @@ const apiRouter = Express.Router();
 // API-Imports
 
 import queueRouter from "./queue/queue";
+import emailsRouter from "./emails/emails";
 
 // Code
 
 apiRouter.use("/queue", queueRouter);
+apiRouter.use("/emails", emailsRouter);
 
 // Exports
 
