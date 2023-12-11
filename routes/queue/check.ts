f96e7aea121caef7c72b800879cda21522ab94bd
LeonTM <LAnneken09@gmail.com>
2023-12-11 10:37:38 +0100

    Fixed clear

--- routes/queue/check.ts
+++ routes/queue/check.ts
@@ -1,22 +1,39 @@
 // Imports
 
 import Express from "express";
-import Job from "../../apiTypes";
+import { Job, LogEvent } from "../../apiTypes";
 import fs from "fs-extra";
+import dotenv from "dotenv";
+import validate from "../validate";
 
 // Presets
 
 const checkRouter = Express.Router();
 const pending: Job[] = fs.readJsonSync("data/queue.json").pending;
-const done: Job[] = fs.readJsonSync("data/queue.json").done;
+const logs: LogEvent[] = fs.readJsonSync("data/logs.json").events;
+let env = dotenv.config();
 
 // Code
 
 checkRouter.get("/", (req, res) => {
-  if (pending.length > 0) {
-    return res.status(201);
+  if (
+    validate(String(req.headers["user"]), String(req.headers["authorization"]))
+  ) {
+    logs.push({
+      type: "checked",
+      user: {
+        name: String(req.headers["user"]),
+      },
+      date: new Date(),
+    });
+    fs.writeJsonSync("data/logs.json", logs, { spaces: 4 });
+    if (pending.length > 0) {
+      return res.status(201);
+    } else {
+      return res.status(404);
+    }
   } else {
-    return res.status(404);
+    return res.status(401);
   }
 });
 
