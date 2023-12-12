032e7e0cd92c3a73c6a4fe8eedf4b280ff09c8ac
Leon™ <leontm.dev@gmail.com>
2023-12-12 21:59:29 +0100

    Added delete Method

--- routes/emails/emails.ts
+++ routes/emails/emails.ts
@@ -9,10 +9,12 @@ const emailsRouter = Express.Router();
 // API-Imports
 
 import addRouter from "./addContact";
+import deleteRouter from "./deleteContact";
 
 // Code
 
 emailsRouter.use("/add", addRouter);
+emailsRouter.use("/delete", deleteRouter);
 
 // Exports
 
