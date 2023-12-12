12025bc7115c60c0eb0dce22c6166c471f81ff2d
Leon™ <leontm.dev@gmail.com>
2023-12-12 21:58:23 +0100

    Added the method for deleting contacts

---
+++ routes/emails/deleteContact.ts
@@ -0,0 +1,44 @@
+// Imports
+
+import Express, { response } from "express";
+import dotenv from "dotenv";
+
+// Presets
+
+let env = dotenv.config();
+const deleteRouter = Express.Router();
+
+// Code
+
+deleteRouter.delete("/email=:email", async (req, res) => {
+  let email = encodeURI(req.params.email);
+  let fetchReq = await fetch(`https://api.brevo.com/v3/contacts/${email}`, {
+    headers: {
+      Accept: "application/json",
+      "api-key": <string>process.env["PUBLIC_KEY"],
+    },
+    method: "DELETE",
+  })
+    .then((fetchRes) => {
+      return fetchRes;
+    })
+    .then((data) => {
+      if (data.status === 204) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .catch((err) => {
+      return false;
+    });
+  if (fetchReq) {
+    res.status(201);
+  } else {
+    res.status(500);
+  }
+});
+
+// Exports
+
+export default deleteRouter;
