0252a31d24789ec7d157147a1fbbf48414773089
Leon™ <leontm.dev@gmail.com>
2023-12-12 22:03:38 +0100

    Added the create method for jobs to the queue API

--- routes/queue/queue.ts
+++ routes/queue/queue.ts
@@ -11,12 +11,14 @@ const queueRouter = Express.Router();
 import checkRouter from "./check";
 import clearRouter from "./clear";
 import { workRouter } from "./workThrough";
+import { createRouter } from "./createJob";
 
 // Code
 
 queueRouter.use("/check", checkRouter);
 queueRouter.use("/clear", clearRouter);
 queueRouter.use("/work", workRouter);
+queueRouter.use("/create", createRouter);
 
 // Exports
 
