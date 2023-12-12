eb5a12db2ca2e28621b9f812805b0a6076c82fc7
Leon™ <leontm.dev@gmail.com>
2023-12-12 20:09:03 +0100

    u

--- server.ts
+++ server.ts
@@ -25,8 +25,26 @@ app.listen(2000, () => {
 
 // API-Imports
 
-// const apiRouter = require("./routes/api");
-
 // Code
 
-// app.use("/api", apiRouter);
+app.use();
+var SibApiV3Sdk = require('sib-api-v3-sdk');
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+var apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = 'YOUR_API_V3_KEY';
+var apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
+var emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
+emailCampaigns.name = "Welcome to the newsletter";
+emailCampaigns.subject = "My subject";
+emailCampaigns.sender = {"name": "finda Newsletter", "email": "news@find-a.app"};
+emailCampaigns.type = "classic";
+emailCampaigns.htmlContent =  'Congratulations! You successfully sent this example campaign via the Brevo API.',
+emailCampaigns.recipients=  {listIds: [2, 7]},
+# Schedule the sending in one hour\
+scheduledAt: '2018-01-01 00:00:01'
+}
+apiInstance.createEmailCampaign(emailCampaigns).then(function(data) {
+console.log('API called successfully. Returned data: ' + data);
+}, function(error) {
+console.error(error);
+});
