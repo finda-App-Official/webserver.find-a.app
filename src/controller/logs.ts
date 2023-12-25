6105ac7540c33265929a0217b884226505646339
Leon™ <leontm.dev@gmail.com>
2023-12-25 18:33:59 +0100

    Removed other reqs and added getAll

--- src/controller/logs.ts
+++ src/controller/logs.ts
@@ -1,47 +1,18 @@
 import { getLogs, createLog, getLogByDate, getLogById } from "../db/logs";
 import express from "express";
 
-export const createL = async (req: express.Request, res: express.Response) => {
-  try {
-    const { type } = req.body;
-
-    if (!type) {
-      return res.sendStatus(400);
-    }
-
-    const log = await createLog({
-      type,
-      date: new Date(),
-    });
-
-    return res.status(200).json(log).end();
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(500);
-  }
-};
-export const getLById = async (req: express.Request, res: express.Response) => {
-  try {
-    const { id } = req.params;
-
-    const log = await getLogById(id);
-
-    return res.json(log).status(200).end();
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(500);
-  }
-};
-export const getLByDate = async (
+export const getAllLogs = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
-    const { date } = req.body;
+    let logs = await getLogs();
 
-    const log = await getLogByDate(date);
+    if (!logs) {
+      return res.status(400);
+    }
 
-    return res.json(log).status(200).end();
+    return res.status(200).json(logs).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
